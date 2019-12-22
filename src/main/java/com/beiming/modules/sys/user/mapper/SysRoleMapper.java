eec72817d1a3152b0315ea1bf3ffca91c0b621f6
刘峰 <liufeng2311@163.com>
2019-12-22 14:16:11 +0800

    修改自动生成工具

--- src/main/java/com/beiming/modules/sys/user/mapper/SysRoleMapper.java
+++
@@ -1,9 +0,0 @@
-package com.beiming.modules.sys.user.mapper;
-
-import com.beiming.modules.base.mapper.BaseMapper;
-import com.beiming.modules.sys.user.domain.entity.SysRole;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface SysRoleMapper extends BaseMapper<SysRole> {
-}
--- src/main/java/com/beiming/modules/sys/user/mapper/SysUserMapper.java
+++
@@ -1,9 +0,0 @@
-package com.beiming.modules.sys.user.mapper;
-
-import com.beiming.modules.base.mapper.BaseMapper;
-import com.beiming.modules.sys.user.domain.entity.SysUser;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface SysUserMapper extends BaseMapper<SysUser> {
-}
